bb4dcebb869c0f183eac8d65e9fbd8807c40fec1
Francisco Andrade <franciscoezequielandrade@gmail.com>
2022-05-21 20:14:28 -0300

    ejercicio 14 te5minado

---
+++ js/ejercicio15.js
@@ -0,0 +1,24 @@
+// 15- Realiza un script que cuente el número de vocales que tiene un texto.
+
+let cadena = prompt("Ingrese el texto para saber las cantidad de vocales que tiene: ");
+let contador=0;
+
+for(let i=0;i<cadena.length;i++){    
+    if(cadena.charAt(i)==='a'){
+        contador++;
+    }
+    if(cadena.charAt(i)==='e'){
+        contador++;
+    }
+    if(cadena.charAt(i)==='i'){
+        contador++;
+    }
+    if(cadena.charAt(i)==='o'){
+        contador++;
+    }
+    if(cadena.charAt(i)==='u'){
+        contador++;
+    }
+}
+
+alert(`tiene ${contador} vocales`)
